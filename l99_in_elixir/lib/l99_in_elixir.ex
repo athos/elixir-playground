c65c27b02daf68564a37e2aa04a6f4d707cf275f
OHTA Shogo <athos0220@gmail.com>
2015-09-27 14:25:58 +0900

    Solve P02

--- l99_in_elixir/lib/l99_in_elixir.ex
+++ l99_in_elixir/lib/l99_in_elixir.ex
@@ -5,4 +5,11 @@ defmodule L99InElixir do
 
     #def my_last(xs), do: Enum.at xs, -1
   end
+
+  defmodule P02 do
+    def my_but_last([_, _] = xs), do: xs
+    def my_but_last([_ | xs]), do: my_but_last xs
+
+    #def my_but_last(xs), do: Enum.slice xs, -2, 2
+  end
 end
--- l99_in_elixir/test/l99_in_elixir_test.exs
+++ l99_in_elixir/test/l99_in_elixir_test.exs
@@ -6,4 +6,8 @@ defmodule L99InElixirTest do
   test "Find the last box of a list." do
     assert L.P01.my_last([:a, :b, :c, :d]) == :d
   end
+
+  test "Find the last but one box of a list." do
+    assert L.P02.my_but_last([:a, :b, :c, :d]) == [:c, :d]
+  end
 end
