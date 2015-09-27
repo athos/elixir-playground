8e393031e44ffc2c8c05b535f31a7e3b7128ae99
OHTA Shogo <athos0220@gmail.com>
2015-09-27 14:15:39 +0900

    Solve P01

--- l99_in_elixir/lib/l99_in_elixir.ex
+++ l99_in_elixir/lib/l99_in_elixir.ex
@@ -1,6 +1,8 @@
 defmodule L99InElixir do
+  defmodule P01 do
+    def my_last([x]), do: x
+    def my_last([_ | xs]), do: my_last xs
+
+    #def my_last(xs), do: Enum.at xs, -1
+  end
 end
-
-
-
-
--- l99_in_elixir/test/l99_in_elixir_test.exs
+++ l99_in_elixir/test/l99_in_elixir_test.exs
@@ -1,8 +1,9 @@
 defmodule L99InElixirTest do
   use ExUnit.Case
   doctest L99InElixir
+  alias L99InElixir, as: L
 
-  test "the truth" do
-    assert 1 + 1 == 1
+  test "Find the last box of a list." do
+    assert L.P01.my_last([:a, :b, :c, :d]) == :d
   end
 end
